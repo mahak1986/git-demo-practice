1ffc6b609c67cbc132fd100af0464bab66ea6702
mahak1986 <agg.mahak1986@gmail.com>
2024-01-27 15:14:06 +0000

    commit3

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println("My name is Mahak,Hello");
         System.out.println("My name is Mahak,How are you?");
     }
 }
